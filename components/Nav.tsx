7176a9113353b7dbf32419d5c194a162f3258007
duck <godqhr2256@gmail.com>
2023-02-24 22:22:25 +0900

    refactor: Nav style 수정

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,16 +1,23 @@
 import navlinks from '../data/navlinks';
 import Link from 'next/link';
+import styled from 'styled-components';
 
 const Nav = () => {
   return (
     <nav>
       {navlinks.map((nav) => (
-        <Link href={nav.link} key={nav.title}>
+        <CustomLink href={nav.link} key={nav.title}>
           {nav.title}
-        </Link>
+        </CustomLink>
       ))}
     </nav>
   );
 };
 
 export default Nav;
+
+const CustomLink = styled(Link)`
+  margin-right: 1.25rem;
+  font-size: 20px;
+  font-weight: bold;
+`;
