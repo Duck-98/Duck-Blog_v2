38f480893fadc918f78db7cc6cb1d92d70fd41f6
duck <godqhr2256@gmail.com>
2023-02-24 22:22:09 +0900

    refactor: Container custom Image 적용

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,9 +2,8 @@ import Head from 'next/head';
 import React, { ReactNode } from 'react';
 import Nav from './Nav';
 import styled from 'styled-components';
-import Image from 'next/image';
 import metadata from '../data/metadata';
-// import Image from './atoms/Image';
+import Image from './atoms/Image';
 
 const Container = (props: { children: React.ReactNode }) => {
   return (
@@ -15,7 +14,8 @@ const Container = (props: { children: React.ReactNode }) => {
         </Head>
         <Header>
           <div className="img-con">
-            <Image src="/home.jpg" alt="Image description" width={40} height={40} />
+            {/* <Image src="/home.jpg" alt="Image description" width={40} height={40} /> */}
+            <Image src="/home.jpg" alt="blog-img" autoSize={false} width={50} height={50} />
             <span className="title">{metadata.title}</span>
           </div>
           <Nav />
