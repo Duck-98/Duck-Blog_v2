070329db329b6441e9f868bf6dc140c15d52d94d
duck <godqhr2256@gmail.com>
2023-02-24 22:23:14 +0900

    refactor: 블로그, 홈화면 style 적용

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import BlogPost from '../components/\bBlogPost';
+import Container from '../components/Container';
+import styled from 'styled-components';
 
 const Blog = () => {
-  return <div>blog</div>;
+  return (
+    <Container>
+      <Div>
+        <BlogPost />
+        <BlogPost />
+        <BlogPost />
+      </Div>
+    </Container>
+  );
 };
+
+const Div = styled.div`
+  margin-top: 2.5rem; /* 40px */
+  display: flex;
+  flex-direction: column;
+`;
 
 export default Blog;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,46 @@
+import React from 'react';
+import Container from '../components/Container';
+import RecentPosts from '../components/RecentPosts';
+import styled from 'styled-components';
+import metadata from '../data/metadata';
+// import Image from 'next/image';
+import Image from '../components/atoms/Image';
+
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <Container>
+      <Div>
+        <div className="img-con">
+          {/* <Image src="/blog.jpg" alt="Image description"  height={40} /> */}
+          <Image src="/blog.jpg" alt="blog-img" autoSize={false} width={768} height={600} />
+          <span className="title">{metadata.title}</span>
+        </div>
+        <RecentPosts />
+      </Div>
+    </Container>
+  );
 };
 
 export default Home;
+
+const Div = styled.div`
+  margin-top: 1.25rem;
+  width: 100%;
+  .img-con {
+    position: relative;
+    width: 100%;
+    .title {
+      position: absolute;
+      font-weight: bolder;
+      top: 3rem;
+      color: white;
+      font-size: 90px;
+      font-style: italic;
+      line-height: 1;
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      filter: drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1));
+    }
+  }
+`;
