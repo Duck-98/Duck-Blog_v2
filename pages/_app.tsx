96c2d0fd4dee9f47d7031efd4e24be1b7094bd4c
duck <godqhr2256@gmail.com>
2023-02-25 23:31:58 +0900

    feat: global Style 적용 및 다크모드 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useState } from 'react';
+// import '../styles/globals.css'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from '../styles/theme';
+import { GlobalStyle } from '../styles/global-style';
+import Container from '../components/Container';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState(darkTheme);
+
+  const toggleTheme = () => {
+    setTheme(theme === lightTheme ? darkTheme : lightTheme);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Container>
+        <Component {...pageProps} toggleTheme={toggleTheme} />
+      </Container>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
