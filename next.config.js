d4223d1c72eed88a45191b87a615352acb6992d2
duck <godqhr2256@gmail.com>
2023-02-26 23:45:44 +0900

    feat: contentLayer 세팅 및 테스트 글 작성

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const { withContentlayer } = require('next-contentlayer');
+
+module.exports = withContentlayer({
+  reactStrictMode: true,
+});
