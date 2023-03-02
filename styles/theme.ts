c1bb363ef3a2b113b9d027a1e2d71f8b515be958
duck <godqhr2256@gmail.com>
2023-03-02 23:55:04 +0900

    style: 코드 테마 색 추가

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,6 +3,7 @@ export const lightTheme = {
   textColor: '#000000',
   toggleBorder: '#FFF',
   gradient: 'linear-gradient(#39598A, #79D7ED)',
+  codeColor: '#FBFCFD',
 };
 
 export const darkTheme = {
@@ -10,6 +11,7 @@ export const darkTheme = {
   textColor: '#FFFFFF',
   toggleBorder: '#6B8096',
   gradient: 'linear-gradient(#091236, #1E215D)',
+  codeColor: '#1e1e1e',
 };
 
 export const theme = {
