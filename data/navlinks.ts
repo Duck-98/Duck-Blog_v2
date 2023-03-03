bdf6424933d0d9b44c12436da559b024f759af33
duck <godqhr2256@gmail.com>
2023-03-04 01:07:53 +0900

    feat: Nav Link 추가

--- data/navlinks.ts
+++ data/navlinks.ts
@@ -1,6 +1,12 @@
 const navlinks: { title: string; link: string }[] = [
   { title: 'Home', link: '/' },
   { title: 'Blog', link: '/blog' },
+  {
+    title: 'About',
+    link: 'https://miniature-river-6e1.notion.site/1f9e3f41d31f49aa954cdb9d1b9bcab4',
+  },
+  { title: 'Velog', link: 'https://velog.io/@dduck' },
+  { title: 'GitHub', link: 'https://github.com/Duck-98' },
 ];
 
 export default navlinks;
