5ab05609905ad0899065ba335655a2a29a757baa
duck <godqhr2256@gmail.com>
2023-03-12 00:21:55 +0900

    feat: 블로그 게시판 페이지네이션 기능 구현

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -3,6 +3,10 @@
 import _1Mdx from './_1.mdx.json' assert { type: 'json' }
 import _10Mdx from './_10.mdx.json' assert { type: 'json' }
 import _11Mdx from './_11.mdx.json' assert { type: 'json' }
+import _12Mdx from './_12.mdx.json' assert { type: 'json' }
+import _13Mdx from './_13.mdx.json' assert { type: 'json' }
+import _14Mdx from './_14.mdx.json' assert { type: 'json' }
+import _15Mdx from './_15.mdx.json' assert { type: 'json' }
 import _2Mdx from './_2.mdx.json' assert { type: 'json' }
 import _3Mdx from './_3.mdx.json' assert { type: 'json' }
 import _4Mdx from './_4.mdx.json' assert { type: 'json' }
@@ -11,9 +15,5 @@ import _6Mdx from './_6.mdx.json' assert { type: 'json' }
 import _7Mdx from './_7.mdx.json' assert { type: 'json' }
 import _8Mdx from './_8.mdx.json' assert { type: 'json' }
 import _9Mdx from './_9.mdx.json' assert { type: 'json' }
-import _12Mdx from './_12.mdx.json' assert { type: 'json' }
-import _13Mdx from './_13.mdx.json' assert { type: 'json' }
-import _14Mdx from './_14.mdx.json' assert { type: 'json' }
-import _15Mdx from './_15.mdx.json' assert { type: 'json' }
 
-export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _2Mdx, _3Mdx, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx]
+export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _2Mdx, _3Mdx, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx]
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,8 +1,9 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { WiDaySunny } from 'react-icons/wi';
 import { MdOutlineModeNight } from 'react-icons/md';
 import { allPosts } from 'contentlayer/generated';
-
+import Pagination from 'react-js-pagination';
 import { lightTheme } from 'styles/theme';
 import { ThemeProp, PostProp } from 'types/type';
 import BlogPost from 'components/\bBlogPost';
@@ -14,6 +15,16 @@ interface Props {
 }
 
 const Blog = ({ toggleTheme, posts, theme }: Props) => {
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState(5);
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+  const itemChange = (e: any) => {
+    setItems(Number(e.target.value));
+  };
+  console.log(items * (page - 1), items * (page - 1) + items);
   return (
     <>
       <BtnWrapper>
@@ -28,15 +39,34 @@ const Blog = ({ toggleTheme, posts, theme }: Props) => {
         )}
       </BtnWrapper>
       <Div>
-        {posts.map((post) => (
-          <BlogPost
-            date={post.date}
-            title={post.title}
-            des={post.description}
-            slug={post._raw.flattenedPath}
-            key={post._id}
-          />
-        ))}
+        <div>
+          <select name="items" onChange={itemChange}>
+            <option value="5">5개</option>
+            <option value="10">10개</option>
+            <option value="15">15개</option>
+            <option value="20">20개</option>
+          </select>
+        </div>
+        {posts.slice(items * (page - 1), items * (page - 1) + items).map((post) => {
+          return (
+            <BlogPost
+              date={post.date}
+              title={post.title}
+              des={post.description}
+              slug={post._raw.flattenedPath}
+              key={post._id}
+            />
+          );
+        })}
+        <PaginationBox>
+          <Pagination
+            activePage={page}
+            itemsCountPerPage={items}
+            totalItemsCount={posts.length - 1}
+            pageRangeDisplayed={5}
+            onChange={handlePageChange}
+          ></Pagination>
+        </PaginationBox>
       </Div>
     </>
   );
@@ -55,6 +85,7 @@ export const getStaticProps = async () => {
 const Div = styled.div`
   display: flex;
   flex-direction: column;
+  padding-bottom: 5rem;
 `;
 const Button = styled.button`
   position: relative;
@@ -66,5 +97,48 @@ const Night = styled(MdOutlineModeNight)`
   color: white;
 `;
 const BtnWrapper = styled.div``;
+
+const PaginationBox = styled.div`
+  .pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  ul.pagination li {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    border: 1px solid #e2e2e2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+  ul.pagination li:first-child {
+    border-radius: 5px 0 0 5px;
+  }
+  ul.pagination li:last-child {
+    border-radius: 0 5px 5px 0;
+  }
+  ul.pagination li a {
+    text-decoration: none;
+    color: ${({ theme }: { theme: any }) => theme.textColor};
+    font-size: 1rem;
+  }
+  ul.pagination li.active a {
+    color: ${({ theme }: { theme: any }) => theme.grayColor};
+  }
+  ul.pagination li.active {
+    background: ${({ theme }: { theme: any }) => theme.textColor};
+  }
+  ul.pagination li a:hover,
+  ul.pagination li a.active {
+  }
+`;
 
 export default Blog;
