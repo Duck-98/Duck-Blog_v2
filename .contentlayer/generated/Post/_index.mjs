7c02a345d80fb31f5b9b22100c8df8c558089047
duck <godqhr2256@gmail.com>
2023-03-20 23:53:10 +0900

    feat: 새 글 작성

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -7,6 +7,9 @@ import _12Mdx from './_12.mdx.json' assert { type: 'json' }
 import _13Mdx from './_13.mdx.json' assert { type: 'json' }
 import _14Mdx from './_14.mdx.json' assert { type: 'json' }
 import _15Mdx from './_15.mdx.json' assert { type: 'json' }
+import _16Mdx from './_16.mdx.json' assert { type: 'json' }
+import _17Mdx from './_17.mdx.json' assert { type: 'json' }
+import _18Mdx from './_18.mdx.json' assert { type: 'json' }
 import _2Mdx from './_2.mdx.json' assert { type: 'json' }
 import _3Mdx from './_3.mdx.json' assert { type: 'json' }
 import _4Mdx from './_4.mdx.json' assert { type: 'json' }
@@ -15,8 +18,6 @@ import _6Mdx from './_6.mdx.json' assert { type: 'json' }
 import _7Mdx from './_7.mdx.json' assert { type: 'json' }
 import _8Mdx from './_8.mdx.json' assert { type: 'json' }
 import _9Mdx from './_9.mdx.json' assert { type: 'json' }
-import _16Mdx from './_16.mdx.json' assert { type: 'json' }
-import _17Mdx from './_17.mdx.json' assert { type: 'json' }
-import _18Mdx from './_18.mdx.json' assert { type: 'json' }
+import _19Mdx from './_19.mdx.json' assert { type: 'json' }
 
-export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _2Mdx, _3Mdx, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx, _16Mdx, _17Mdx, _18Mdx]
+export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _2Mdx, _3Mdx, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx, _19Mdx]
