f30ed6710228ba9875a5e149a6f8b23f57b52160
duck <godqhr2256@gmail.com>
2023-03-25 22:36:07 +0900

    refactor: 사이트맵 설정 변경

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,24 @@
+// module.exports = {
+//   siteUrl: 'https://duck-blog-v2-duck-98.vercel.app/',
+//   generateRobotsTxt: true,
+// };
+
 module.exports = {
-  siteUrl: 'https://duck-blog-v2-duck-98.vercel.app/',
-  generateRobotsTxt: true,
+  siteUrl: 'https://duck-blog-v2-duck-98.vercel.app/' || 'https://localhost:3000/',
+  generateRobotsTxt: true, // (optional)
+  exclude: ['/404'], // <= exclude here
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/404'],
+      },
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [
+      `${
+        'https://duck-blog-v2-duck-98.vercel.app/' || 'https://localhost:3000/'
+      }server-sitemap.xml`, // <==== Add here
+    ],
+  },
 };
