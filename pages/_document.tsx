19e006f18c93e3d0578d1c0e19517d2ec808ea6d
duck <godqhr2256@gmail.com>
2023-03-26 23:50:32 +0900

    refactor: 구글 사이트맵 등록을 위한 메타태그 등록

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,29 @@
-import Document, { DocumentContext } from 'next/document';
+import Document, { DocumentContext, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta
+            name="google-site-verification"
+            content="_fboZ-uPoMducC2AS-qhRCH_nW6RcLe8gvl-MvL1JHk"
+          />
+        </Head>
+        <body>
+          <div id="root">
+            <Main />
+            <NextScript />
+          </div>
+        </body>
+      </html>
+    );
+  }
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
