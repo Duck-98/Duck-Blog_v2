deee595a960ce3f37b8b15c07803ff63559d4d3c
duck <godqhr2256@gmail.com>
2023-04-25 22:15:00 +0900

    feat: CS[클로저]글 작성

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -24,5 +24,6 @@ import _6Mdx from './_6.mdx.json' assert { type: 'json' }
 import _7Mdx from './_7.mdx.json' assert { type: 'json' }
 import _8Mdx from './_8.mdx.json' assert { type: 'json' }
 import _9Mdx from './_9.mdx.json' assert { type: 'json' }
+import _25Mdx from './_25.mdx.json' assert { type: 'json' }
 
-export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _19Mdx, _2Mdx, _20Mdx, _21Mdx, _22Mdx, _23Mdx, _24Mdx, _3Mdx, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx]
+export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _19Mdx, _2Mdx, _20Mdx, _21Mdx, _22Mdx, _23Mdx, _24Mdx, _3Mdx, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx, _25Mdx]
