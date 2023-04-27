ab661df5f478e767ab83802bff37a7a668286c62
duck <godqhr2256@gmail.com>
2023-04-27 23:25:52 +0900

    feat: 링크 공유 적용

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -4,9 +4,13 @@ import { allPosts } from 'contentlayer/generated';
 import { InferGetStaticPropsType } from 'next';
 import styled from 'styled-components';
 import MDXComponent from 'components/atoms/MDXComponent';
+import share from '~/utils/share';
+import { useRouter } from 'next/router';
+import { FiShare } from 'react-icons/fi';
 
 const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
   // const MDXComponent = useMDXComponent(post.body.code);
+  const router = useRouter();
 
   const customMeta = {
     title: post.title,
@@ -14,11 +18,33 @@ const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
     date: new Date(post.date).toISOString(),
   };
 
+  const data = {
+    title: post.title,
+    text: post.description,
+    url: `https://duck-blog-v2-duck-98.vercel.app${router.asPath}`,
+  };
+
+  const handleShare = async () => {
+    const result = await share(data);
+    console.log(result);
+    if (result === 'copiedToClipboard') {
+      alert('링크를 클립보드에 복사했습니다.');
+    } else if (result === 'failed') {
+      alert('공유하기가 지원되지 않는 환경입니다.');
+    }
+  };
+
   return (
     // <Container customMeta={customMeta}>
     <Div>
       <h1>{post!.title}</h1>
       <MDXComponent mdx={post!.body.code} />
+      <div className="btn-con">
+        <Button onClick={handleShare}>
+          <Icon /> 공유
+        </Button>
+      </div>
+
       <Comments />
     </Div>
     // </Container>
@@ -48,6 +74,27 @@ const Div = styled.div`
     color: rgb(3 105 161);
     padding: 20px;
   }
+  .btn-con {
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+
+const Icon = styled(FiShare)`
+  width: 3rem;
+  height: 3rem;
+`;
+
+const Button = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${({ theme }: { theme: any }) => theme.textColor};
+  font-size: 1rem;
+  width: 4rem;
+  height: 4rem;
+  border: none;
+  cursor: pointer;
 `;
 
 export default Post;
