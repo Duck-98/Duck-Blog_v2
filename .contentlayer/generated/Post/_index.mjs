5b97fe594e18c1ca710e07c890a401d25404e5e9
duck <godqhr2256@gmail.com>
2023-04-27 21:14:50 +0900

    chore: og image 추가 및 자잘한 코드

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -17,6 +17,7 @@ import _21Mdx from './_21.mdx.json' assert { type: 'json' }
 import _22Mdx from './_22.mdx.json' assert { type: 'json' }
 import _23Mdx from './_23.mdx.json' assert { type: 'json' }
 import _24Mdx from './_24.mdx.json' assert { type: 'json' }
+import _25Mdx from './_25.mdx.json' assert { type: 'json' }
 import _3Mdx from './_3.mdx.json' assert { type: 'json' }
 import _4Mdx from './_4.mdx.json' assert { type: 'json' }
 import _5Mdx from './_5.mdx.json' assert { type: 'json' }
@@ -24,6 +25,5 @@ import _6Mdx from './_6.mdx.json' assert { type: 'json' }
 import _7Mdx from './_7.mdx.json' assert { type: 'json' }
 import _8Mdx from './_8.mdx.json' assert { type: 'json' }
 import _9Mdx from './_9.mdx.json' assert { type: 'json' }
-import _25Mdx from './_25.mdx.json' assert { type: 'json' }
 
-export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _19Mdx, _2Mdx, _20Mdx, _21Mdx, _22Mdx, _23Mdx, _24Mdx, _3Mdx, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx, _25Mdx]
+export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _19Mdx, _2Mdx, _20Mdx, _21Mdx, _22Mdx, _23Mdx, _24Mdx, _25Mdx, _3Mdx, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx]
