3bb8031015f47bdfc45902bbeece45edb68a449d
duck <godqhr2256@gmail.com>
2023-05-06 19:41:02 +0900

    feat: 테마 모드 별 댓글 테마 적용

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,35 +1,56 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 
 const Comments = () => {
   const [current, setCurrent] = useState<string>('dark');
 
-  // useEffect(() => {
-  //   const item = window.localStorage.getItem('theme');
-  //   if (JSON.parse(item).bgColor === '#060606') {
-  //     setCurrent('light');
-  //   } else {
-  //     setCurrent('dark');
-  //   }
-  // }, []);
-
+  useEffect(() => {
+    const item = window.localStorage.getItem('theme');
+    if (item === 'light') {
+      setCurrent('light');
+    } else {
+      setCurrent('dark');
+    }
+  }, []);
   return (
-    <section
-      ref={(elem) => {
-        if (!elem) {
-          return;
-        }
-        const scriptElem = document.createElement('script');
-        scriptElem.src = 'https://utteranc.es/client.js';
-        scriptElem.async = true;
-        scriptElem.setAttribute('repo', 'Duck-98/duck-blog-comment');
-        scriptElem.setAttribute('issue-term', 'pathname');
-        // scriptElem.setAttribute('theme', `github-${current}`);
-        scriptElem.setAttribute('theme', `github-dark`);
-        scriptElem.setAttribute('label', 'blog-comment');
-        scriptElem.crossOrigin = 'anonymous';
-        elem.appendChild(scriptElem);
-      }}
-    />
+    <>
+      {current === 'dark' ? (
+        <>
+          <section
+            ref={(elem) => {
+              if (!elem) {
+                return;
+              }
+              const scriptElem = document.createElement('script');
+              scriptElem.src = 'https://utteranc.es/client.js';
+              scriptElem.async = true;
+              scriptElem.setAttribute('repo', 'Duck-98/duck-blog-comment');
+              scriptElem.setAttribute('issue-term', 'pathname');
+              scriptElem.setAttribute('theme', `github-dark`);
+              scriptElem.setAttribute('label', 'blog-comment');
+              scriptElem.crossOrigin = 'anonymous';
+              elem.appendChild(scriptElem);
+            }}
+          />
+        </>
+      ) : (
+        <section
+          ref={(elem) => {
+            if (!elem) {
+              return;
+            }
+            const scriptElem = document.createElement('script');
+            scriptElem.src = 'https://utteranc.es/client.js';
+            scriptElem.async = true;
+            scriptElem.setAttribute('repo', 'Duck-98/duck-blog-comment');
+            scriptElem.setAttribute('issue-term', 'pathname');
+            scriptElem.setAttribute('theme', `github-light`);
+            scriptElem.setAttribute('label', 'blog-comment');
+            scriptElem.crossOrigin = 'anonymous';
+            elem.appendChild(scriptElem);
+          }}
+        />
+      )}
+    </>
   );
 };
 
