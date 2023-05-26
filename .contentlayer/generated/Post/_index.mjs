3e7cd9861614c10352ad381492f89f9b2a38a197
duck <godqhr2256@gmail.com>
2023-05-26 21:17:33 +0900

    feat: 새 글 작성

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -31,5 +31,6 @@ import _6Mdx from './_6.mdx.json' assert { type: 'json' }
 import _7Mdx from './_7.mdx.json' assert { type: 'json' }
 import _8Mdx from './_8.mdx.json' assert { type: 'json' }
 import _9Mdx from './_9.mdx.json' assert { type: 'json' }
+import _32Md from './_32.md.json' assert { type: 'json' }
 
-export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _19Mdx, _2Mdx, _20Mdx, _21Mdx, _22Mdx, _23Mdx, _24Mdx, _25Mdx, _26Md, _27Md, _28Md, _29Md, _3Mdx, _30Md, _31Md, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx]
+export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _19Mdx, _2Mdx, _20Mdx, _21Mdx, _22Mdx, _23Mdx, _24Mdx, _25Mdx, _26Md, _27Md, _28Md, _29Md, _3Mdx, _30Md, _31Md, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx, _32Md]
