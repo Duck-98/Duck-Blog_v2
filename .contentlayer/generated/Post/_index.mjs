59a2e5a86ade8f977e8033b28d82b2390307b7ee
duck <godqhr2256@gmail.com>
2023-11-24 21:46:20 +0900

    fix: gitignore 수정

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -25,12 +25,14 @@ import _29Md from './_29.md.json' assert { type: 'json' }
 import _3Mdx from './_3.mdx.json' assert { type: 'json' }
 import _30Md from './_30.md.json' assert { type: 'json' }
 import _31Md from './_31.md.json' assert { type: 'json' }
+import _32Md from './_32.md.json' assert { type: 'json' }
+import _33Md from './_33.md.json' assert { type: 'json' }
+import _34Md from './_34.md.json' assert { type: 'json' }
 import _4Mdx from './_4.mdx.json' assert { type: 'json' }
 import _5Mdx from './_5.mdx.json' assert { type: 'json' }
 import _6Mdx from './_6.mdx.json' assert { type: 'json' }
 import _7Mdx from './_7.mdx.json' assert { type: 'json' }
 import _8Mdx from './_8.mdx.json' assert { type: 'json' }
 import _9Mdx from './_9.mdx.json' assert { type: 'json' }
-import _32Md from './_32.md.json' assert { type: 'json' }
 
-export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _19Mdx, _2Mdx, _20Mdx, _21Mdx, _22Mdx, _23Mdx, _24Mdx, _25Mdx, _26Md, _27Md, _28Md, _29Md, _3Mdx, _30Md, _31Md, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx, _32Md]
+export const allPosts = [_1Mdx, _10Mdx, _11Mdx, _12Mdx, _13Mdx, _14Mdx, _15Mdx, _16Mdx, _17Mdx, _18Mdx, _19Mdx, _2Mdx, _20Mdx, _21Mdx, _22Mdx, _23Mdx, _24Mdx, _25Mdx, _26Md, _27Md, _28Md, _29Md, _3Mdx, _30Md, _31Md, _32Md, _33Md, _34Md, _4Mdx, _5Mdx, _6Mdx, _7Mdx, _8Mdx, _9Mdx]
