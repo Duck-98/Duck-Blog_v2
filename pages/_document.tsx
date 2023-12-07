0891d6fd422d9d375cbed55efabe00a3ad10bd8d
Deokgyung <devduck@mailplug.co.kr>
2023-12-07 10:49:24 +0900

    feat: 네이버 서치 등록

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,8 @@ export default class MyDocument extends Document {
             name="google-site-verification"
             content="_fboZ-uPoMducC2AS-qhRCH_nW6RcLe8gvl-MvL1JHk"
           />
+          <meta name="naver-site-verification" content="200c87f231960d7ce0142f8c97120f1db44a123e" />
+
           <meta name="robots" content="all"></meta>
         </Head>
         <body>
